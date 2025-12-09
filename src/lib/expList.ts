ed5644841cc376b50db630d4003922f483462deb
sd2k5 <s.2k5@gmail.com>
2025-12-09 23:01:03 +0530

    Added new experience and refactored types

---
+++ src/lib/expList.ts
@@ -0,0 +1,16 @@
+import type { Experience } from "../types/Experience";
+
+export const expList: Experience[] = [
+  {
+    position: "SDE Intern",
+    org: "Schneider Electric",
+    desc: "Designed and developed a quality assessment tool to evaluate metrics from static code analyzer tools, improving report generation workflows for QA teams.",
+    date: "18 Aug 2025 - 24 Nov 2025",
+  },
+  {
+    position: "Frontend Developer",
+    org: "Resourcio",
+    desc: "Working as a frontend developer at Resourcio, gaining hands-on experience with Next.js and Tailwind.",
+    date: "31 Aug 2025",
+  },
+];
---
+++ src/types/Experience.ts
@@ -0,0 +1,6 @@
+export type Experience = {
+  position: string;
+  org: string;
+  desc: string;
+  date: string;
+};
